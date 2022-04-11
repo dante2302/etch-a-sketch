773cc3a011bb2014d09dd9d3c4bdf56ddca663f6
Dante <guchoo923@gmail.com>
2022-04-11 21:20:05 +0300

    Declare doc variables, write getSize function

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const container = document.querySelector('.container');
 const rainbowButton = document.querySelector('.rainbow');
 const clearButton = document.querySelector('.clear');
-const 
+const eraser = document.querySelector('.eraser');
+
+
+function getSize(){
+    let size  = prompt("Declare the size of your board: ", 1)
+    if(size>100){
+        return alert('ERROR')
+    }
+    return size;
+}
