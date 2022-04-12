304bd17c16f25ef8dcec8cebf3eb5502f0c48b17
Dante <guchoo923@gmail.com>
2022-04-12 21:22:41 +0300

    Implement changeColor function

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const gridLinesButton = document.querySelector('.grid-lines');
 const pane = document.querySelector('.container');
 const eraser = document.querySelector('.eraser');
 
+const defaultColor = "black"
+let newElements = []
+
 function getSize(){
     let size  = prompt("Declare the size of your board: ", 1) //get size
     parseInt(size,10) // convert it to a number
@@ -15,11 +18,14 @@ function getSize(){
     return size;
 }
 
+
 function makePane(){
+    while(pane.firstChild){
+        pane.removeChild(pane.lastChild)
+    }
     let size = getSize(); 
     pane.style.gridTemplateColumns = `repeat(${size}, auto)`;
     pane.style.gridTemplateRows = `repeat(${size}, auto)`;
-    let newElements = []; // declare an array for the new divs
     for(let i = 0;i<size*size;i++){
         newElements[i] = document.createElement('div');
         newElements[i].classList.add('default');
@@ -28,28 +34,49 @@ function makePane(){
         pane.appendChild(newElements[i])
     }// append new divs
     for(let i = 0;i<size*size;i++){
-        newElements[i].addEventListener('dragover',()=>{
-
-            newElements[i].style.backgroundColor = "red"})
+        newElements[i].addEventListener('mouseover',()=>{def(newElements,i,defaultColor)})
+        newElements[i].addEventListener('mousedown',()=>{def(newElements,i,defaultColor)})
     }// add event listeners to new divs
 }   
-function clearPane(){
-    let count = pane.childElementCount;
-    while(pane.firstChild){
-        pane.removeChild(pane.lastChild)
-    }
-    makePane();
+function def(arr,i,color){
+    arr[i].style.backgroundColor = `${color}`
 }
-function toggleLines(){
-    pane.children.classList.toggle('grid-lines');
-}
-function eventListeners(){
 
+function clearPane(){
+    let count = pane.childElementCount
+    for(let i = 0;i<count;i++){
+        pane.childNodes[i].style.backgroundColor = "white";
+    }
 }
+
+function toggleLines(){
+    for(let i = 0;i<pane.childNodes.length;i++)
+    pane.childNodes[i].classList.toggle('grid-lines');
+}
+
+
+// function setErasor(){
+//     for(let i = 0;i<pane.childElementCount;i++){
+//         pane.childNodes[i].addEventListener('click', (e)=>)
+//     }
+// }
 
 sizeButton.addEventListener('click', ()=>makePane())
 clearButton.addEventListener('click', ()=>clearPane())
 gridLinesButton.addEventListener('click', ()=>toggleLines());
+eraser.addEventListener('click', ()=>setErasor())
+colorPicker.onchange = (e) => changeColor(e.target.value)
+function changeColor(value){
+    count = pane.childElementCount;
+    for(let i = 0;i<count;i++){
+        pane.childNodes[i].removeEventListener('mouseover',()=>{def(newElements,i,defaultColor)})
+        pane.childNodes[i].removeEventListener('mousedown',()=>{def(newElements,i,defaultColor)})
 
+    }
+    for(let i = 0;i<count;i++){
+        pane.childNodes[i].addEventListener('mouseover',()=>{def(newElements,i,value)})
+        pane.childNodes[i].addEventListener('mousedown',()=>{def(newElements,i,value)})
 
-
+    }
+}
+changeColor("#001524")
